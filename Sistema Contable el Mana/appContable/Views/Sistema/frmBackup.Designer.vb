54064b8b3245ca5f66bc7e4c6dd81921b9b51d45
Michael Traña <mtraatabladaa94@gmail.com>
2016-11-27 22:53:04 -0600

    Avances de la Herramienta Backup
    1. Crear Respaldos
    2. Modificar Carpeta de Respaldos

--- Sistema Contable el Mana/appContable/Views/Sistema/frmBackup.Designer.vb
+++ Sistema Contable el Mana/appContable/Views/Sistema/frmBackup.Designer.vb
@@ -30,15 +30,15 @@ Partial Class frmBackup
         Me.MaterialTabSelector1 = New MaterialSkin.Controls.MaterialTabSelector()
         Me.MaterialTabControl1 = New MaterialSkin.Controls.MaterialTabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
-        Me.MaterialSingleLineTextField1 = New MaterialSkin.Controls.MaterialSingleLineTextField()
+        Me.btCancelar = New MaterialSkin.Controls.MaterialFlatButton()
+        Me.btCrearRespaldo = New MaterialSkin.Controls.MaterialRaisedButton()
         Me.btExaminar = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.txtDirectorio = New MaterialSkin.Controls.MaterialSingleLineTextField()
+        Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.dtRegistro = New DevComponents.DotNetBar.Controls.DataGridViewX()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.fdbExplorador = New System.Windows.Forms.FolderBrowserDialog()
-        Me.btCrearRespaldo = New MaterialSkin.Controls.MaterialRaisedButton()
-        Me.btCancelar = New MaterialSkin.Controls.MaterialFlatButton()
         Me.Panel1.SuspendLayout()
         Me.MaterialTabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
@@ -83,7 +83,7 @@ Partial Class frmBackup
         Me.TabPage1.Controls.Add(Me.btCancelar)
         Me.TabPage1.Controls.Add(Me.btCrearRespaldo)
         Me.TabPage1.Controls.Add(Me.btExaminar)
-        Me.TabPage1.Controls.Add(Me.MaterialSingleLineTextField1)
+        Me.TabPage1.Controls.Add(Me.txtDirectorio)
         Me.TabPage1.Controls.Add(Me.MaterialLabel1)
         Me.TabPage1.Location = New System.Drawing.Point(4, 23)
         Me.TabPage1.Name = "TabPage1"
@@ -93,16 +93,60 @@ Partial Class frmBackup
         Me.TabPage1.Text = "Crear Respaldos"
         Me.TabPage1.UseVisualStyleBackColor = True
         '
-        'TabPage2
+        'btCancelar
         '
-        Me.TabPage2.Controls.Add(Me.dtRegistro)
-        Me.TabPage2.Location = New System.Drawing.Point(4, 23)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(546, 294)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Puntos de Restauración"
-        Me.TabPage2.UseVisualStyleBackColor = True
+        Me.btCancelar.AutoSize = True
+        Me.btCancelar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btCancelar.Depth = 0
+        Me.btCancelar.Location = New System.Drawing.Point(264, 56)
+        Me.btCancelar.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.btCancelar.MouseState = MaterialSkin.MouseState.HOVER
+        Me.btCancelar.Name = "btCancelar"
+        Me.btCancelar.Primary = False
+        Me.btCancelar.Size = New System.Drawing.Size(82, 36)
+        Me.btCancelar.TabIndex = 4
+        Me.btCancelar.Text = "Cancelar"
+        Me.btCancelar.UseVisualStyleBackColor = True
+        '
+        'btCrearRespaldo
+        '
+        Me.btCrearRespaldo.Depth = 0
+        Me.btCrearRespaldo.Location = New System.Drawing.Point(19, 56)
+        Me.btCrearRespaldo.MouseState = MaterialSkin.MouseState.HOVER
+        Me.btCrearRespaldo.Name = "btCrearRespaldo"
+        Me.btCrearRespaldo.Primary = True
+        Me.btCrearRespaldo.Size = New System.Drawing.Size(238, 36)
+        Me.btCrearRespaldo.TabIndex = 3
+        Me.btCrearRespaldo.Text = "Crear Punto de Restauración"
+        Me.btCrearRespaldo.UseVisualStyleBackColor = True
+        '
+        'btExaminar
+        '
+        Me.btExaminar.Depth = 0
+        Me.btExaminar.Location = New System.Drawing.Point(501, 11)
+        Me.btExaminar.MouseState = MaterialSkin.MouseState.HOVER
+        Me.btExaminar.Name = "btExaminar"
+        Me.btExaminar.Primary = True
+        Me.btExaminar.Size = New System.Drawing.Size(30, 30)
+        Me.btExaminar.TabIndex = 2
+        Me.btExaminar.Text = "..."
+        Me.btExaminar.UseVisualStyleBackColor = True
+        '
+        'txtDirectorio
+        '
+        Me.txtDirectorio.Depth = 0
+        Me.txtDirectorio.Enabled = False
+        Me.txtDirectorio.Hint = ""
+        Me.txtDirectorio.Location = New System.Drawing.Point(187, 14)
+        Me.txtDirectorio.MouseState = MaterialSkin.MouseState.HOVER
+        Me.txtDirectorio.Name = "txtDirectorio"
+        Me.txtDirectorio.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txtDirectorio.SelectedText = ""
+        Me.txtDirectorio.SelectionLength = 0
+        Me.txtDirectorio.SelectionStart = 0
+        Me.txtDirectorio.Size = New System.Drawing.Size(316, 23)
+        Me.txtDirectorio.TabIndex = 1
+        Me.txtDirectorio.UseSystemPasswordChar = False
         '
         'MaterialLabel1
         '
@@ -117,33 +161,16 @@ Partial Class frmBackup
         Me.MaterialLabel1.TabIndex = 0
         Me.MaterialLabel1.Text = "Directorio de Respaldo:"
         '
-        'MaterialSingleLineTextField1
+        'TabPage2
         '
-        Me.MaterialSingleLineTextField1.Depth = 0
-        Me.MaterialSingleLineTextField1.Enabled = False
-        Me.MaterialSingleLineTextField1.Hint = ""
-        Me.MaterialSingleLineTextField1.Location = New System.Drawing.Point(187, 14)
-        Me.MaterialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER
-        Me.MaterialSingleLineTextField1.Name = "MaterialSingleLineTextField1"
-        Me.MaterialSingleLineTextField1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
-        Me.MaterialSingleLineTextField1.SelectedText = ""
-        Me.MaterialSingleLineTextField1.SelectionLength = 0
-        Me.MaterialSingleLineTextField1.SelectionStart = 0
-        Me.MaterialSingleLineTextField1.Size = New System.Drawing.Size(316, 23)
-        Me.MaterialSingleLineTextField1.TabIndex = 1
-        Me.MaterialSingleLineTextField1.UseSystemPasswordChar = False
-        '
-        'btExaminar
-        '
-        Me.btExaminar.Depth = 0
-        Me.btExaminar.Location = New System.Drawing.Point(501, 11)
-        Me.btExaminar.MouseState = MaterialSkin.MouseState.HOVER
-        Me.btExaminar.Name = "btExaminar"
-        Me.btExaminar.Primary = True
-        Me.btExaminar.Size = New System.Drawing.Size(30, 30)
-        Me.btExaminar.TabIndex = 2
-        Me.btExaminar.Text = "..."
-        Me.btExaminar.UseVisualStyleBackColor = True
+        Me.TabPage2.Controls.Add(Me.dtRegistro)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 23)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(546, 294)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Puntos de Restauración"
+        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'dtRegistro
         '
@@ -169,33 +196,6 @@ Partial Class frmBackup
         Me.Panel2.Name = "Panel2"
         Me.Panel2.Size = New System.Drawing.Size(713, 324)
         Me.Panel2.TabIndex = 1
-        '
-        'btCrearRespaldo
-        '
-        Me.btCrearRespaldo.Depth = 0
-        Me.btCrearRespaldo.Location = New System.Drawing.Point(19, 56)
-        Me.btCrearRespaldo.MouseState = MaterialSkin.MouseState.HOVER
-        Me.btCrearRespaldo.Name = "btCrearRespaldo"
-        Me.btCrearRespaldo.Primary = True
-        Me.btCrearRespaldo.Size = New System.Drawing.Size(238, 36)
-        Me.btCrearRespaldo.TabIndex = 3
-        Me.btCrearRespaldo.Text = "Crear Punto de Restauración"
-        Me.btCrearRespaldo.UseVisualStyleBackColor = True
-        '
-        'btCancelar
-        '
-        Me.btCancelar.AutoSize = True
-        Me.btCancelar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.btCancelar.Depth = 0
-        Me.btCancelar.Location = New System.Drawing.Point(264, 56)
-        Me.btCancelar.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
-        Me.btCancelar.MouseState = MaterialSkin.MouseState.HOVER
-        Me.btCancelar.Name = "btCancelar"
-        Me.btCancelar.Primary = False
-        Me.btCancelar.Size = New System.Drawing.Size(82, 36)
-        Me.btCancelar.TabIndex = 4
-        Me.btCancelar.Text = "Cancelar"
-        Me.btCancelar.UseVisualStyleBackColor = True
         '
         'frmBackup
         '
@@ -229,7 +229,7 @@ Partial Class frmBackup
     Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
     Friend WithEvents MaterialTabSelector1 As MaterialSkin.Controls.MaterialTabSelector
     Friend WithEvents MaterialLabel1 As MaterialSkin.Controls.MaterialLabel
-    Friend WithEvents MaterialSingleLineTextField1 As MaterialSkin.Controls.MaterialSingleLineTextField
+    Friend WithEvents txtDirectorio As MaterialSkin.Controls.MaterialSingleLineTextField
     Friend WithEvents btExaminar As MaterialSkin.Controls.MaterialRaisedButton
     Friend WithEvents dtRegistro As DevComponents.DotNetBar.Controls.DataGridViewX
     Friend WithEvents Panel2 As System.Windows.Forms.Panel
--- Sistema Contable el Mana/appContable/Views/Sistema/frmBackup.vb
+++ Sistema Contable el Mana/appContable/Views/Sistema/frmBackup.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports appControllers
 
 Public Class frmBackup
 
@@ -21,10 +22,28 @@ Public Class frmBackup
 
     Private Sub btExaminar_Click(sender As Object, e As EventArgs) Handles btExaminar.Click
         Try
-            
+            Try
+                fdbExplorador.SelectedPath = BackUp.Path
+            Catch
+            End Try
+
             If fdbExplorador.ShowDialog() = Windows.Forms.DialogResult.OK Then
 
             End If
+        Catch ex As Exception
+            Config.MsgErr(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btCancelar_Click(sender As Object, e As EventArgs) Handles btCancelar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btCrearRespaldo_Click(sender As Object, e As EventArgs) Handles btCrearRespaldo.Click
+        Try
+            BackUp.BackUpCreate(txtDirectorio.Text)
+            txtDirectorio.Clear()
+            Config.MsgInfo("Respaldo creado correctamente")
         Catch ex As Exception
             Config.MsgErr(ex.Message)
         End Try
